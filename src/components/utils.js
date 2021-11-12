59de733dd5aa59beb1be1972345d06b35be022f8
Kirill Shipilov <shipilov20@yandex.ru>
2021-11-12 22:13:12 +0400

    fix after revue two

--- src/components/utils.js
+++ src/components/utils.js
@@ -1,21 +1,3 @@
-import {
-  profileName,
-  profilehobby,
-  profileAvatar,
-} from "../components/constants";
-import { getInitialUser } from "../components/api";
-
-export let userId;
-
-export const loadingProfile = () => {
-  getInitialUser().then((res) => {
-    userId = res._id;
-    profileName.textContent = res.name;
-    profilehobby.textContent = res.about;
-    profileAvatar.src = res.avatar;
-  });
-};
-
 export const savingText = (load, button) =>
   load
     ? (button.textContent = "Сохранение...")
